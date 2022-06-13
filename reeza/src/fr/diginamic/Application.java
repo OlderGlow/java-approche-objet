3df46315192eddbc1caba90dd781be62edb64eeb
Julien Picquet <julienpicquet1@gmail.com>
2022-06-13 11:17:46 +0200

    add tp reeza

--- reeza/src/fr/diginamic/Application.java
+++ reeza/src/fr/diginamic/Application.java
@@ -6,7 +6,7 @@ import fr.diginamic.exception.AppartementDejaReserveException;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) throws AppartementDejaReserveException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int choix = 0;
         do {
