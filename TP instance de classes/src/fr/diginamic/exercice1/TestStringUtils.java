3ec7aedf482870eb167094508da7655d94d169a7
Julien Picquet <julienpicquet1@gmail.com>
2022-06-13 09:57:26 +0200

    add tp 15 ex1

---
+++ TP instance de classes/src/fr/diginamic/exercice1/TestStringUtils.java
@@ -0,0 +1,9 @@
+package fr.diginamic.exercice1;
+
+import fr.diginamic.exercice1.utils.StringUtils;
+
+public class TestStringUtils {
+    public static void main(String[] args) {
+        StringUtils.asciiArt("Hello World");
+    }
+}
